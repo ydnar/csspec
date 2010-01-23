f6a9d85a96c246e97fad438facd36a78df03d0f9
ydnar <ydnar@shaderlab.com>
2010-01-23 12:47:06 -0800

    Define CSSpec as an object.

--- csspec2.js
+++ csspec2.js
@@ -1,5 +1,20 @@
 (function(){
-  var CSSpec = {};
+  var CSSpec = {
+    examples: [],
+
+    describe: function(desc) {
+      return CSSpec.addExample(new CSSpec.ExampleGroup(desc));
+    },
+    
+    addExample: function(example) {
+      this.examples.push(example);
+      return example;
+    },
+
+    expect: function(value) {
+      return new CSSpec.Matchers(value)
+    }
+  };
 
 
   CSSpec.Example = function(desc, fn) {
@@ -88,15 +103,6 @@
       if(!(this.value == expected))
         throw "Expected " + expected + " but got " + this.value;
     }
-  };
-
-  
-  CSSpec.describe = function(desc) {
-    return new CSSpec.ExampleGroup(desc);
-  };
-  
-  CSSpec.expect = function(value) {
-    return new CSSpec.Matchers(value)
   };
 
 
